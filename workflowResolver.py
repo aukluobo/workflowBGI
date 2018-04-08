23de01db598b23790a3c28fccb509e63016db5f5
[huangguodong] <[huangguodong@genomics.cn>
2018-04-08 20:49:37 +0800

    add function in class

--- workflowResolver.py
+++ workflowResolver.py
@@ -1,10 +1,24 @@
 import os
 import sys
-
 import WGS
 
-class workflowResolver:
-    #
+#import jobexcutor
+
+class workflowResolver():
+    def __init__(self):
+        self.workflowName=""
+        self.workflowJson=""
+        self.queue=""
+        self.project=""
+        self.wgs=WGS()
+        self.allflow={'WGS':self.wgs}
+    def loadworkflow(self, workflowName,workflowJson=None):
+        workflowparser=allflow[workflowName]
+        if workflowJson is None:
+            workflowparser.dumpjson()
+        else:
+            workflowparser.loadjson()
+
 
 
 
