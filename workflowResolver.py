f587f049c513cdfbf3f49b078b5ea6d20f069295
[huangguodong] <[huangguodong@genomics.cn>
2018-04-09 20:48:48 +0800

    write the job running part

--- workflowResolver.py
+++ workflowResolver.py
@@ -1,8 +1,9 @@
 import os
 import sys
 import WGS
+import logging
 
-#import jobexcutor
+import jobexcutor
 
 class workflowResolver():
     def __init__(self):
@@ -10,14 +11,32 @@ class workflowResolver():
         self.workflowJson=""
         self.queue=""
         self.project=""
-        self.wgs=WGS()
-        self.allflow={'WGS':self.wgs}
     def loadworkflow(self, workflowName,workflowJson=None):
-        workflowparser=allflow[workflowName]
+        workflowparser=eval(workflowName).interface()
+        jsoncontent={}
         if workflowJson is None:
             workflowparser.dumpjson()
+            jsoncontent=workflowparser.loadjson()
         else:
-            workflowparser.loadjson()
+            try:
+                jsoncontent=workflowparser.loadjson(inputfile=workflowJson)
+            except ValueError as e:
+                raise e
+        for step in workflowparser.step:
+            if step in jsoncontent:
+                stepc=eval(workflowName+'.'+step)
+                stepc.parameter=jsoncontent[step]['parameter']
+                stepc.program=jsoncontent[step]['program']
+                stepc.outdirMain=jsoncontent['outdir']
+                commandshell,out1,out2=stepc.makeCommand(jsoncontent[step]['input'][0],jsoncontent[step]['input'][1])
+                logging.info("output:"+out1+"\n"+out2)
+                runjob=jobexcutor.jobexecutor()
+                runjob.runclusterjob(commandshell,stepc.outdirMain,jsoncontent[step]['resource'])
+
+                
+
+
+        
 
 
 
