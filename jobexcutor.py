c6ed2c0214baad46dc7f9a1e5d0d26c199fb5b0f
[huangguodong] <[huangguodong@genomics.cn>
2018-04-10 19:52:23 +0800

    add multiple function to run job

--- jobexcutor.py
+++ jobexcutor.py
@@ -1,36 +1,123 @@
 import os
 import sys
 import re
+import subprocess
 import argparse
 import logging
 import json
+import time
 
 class jobexecutor:
     def __init__(self):
         self.command="echo please set command"
         self.outdir=os.path.abspath('.')
         self.input="%s/state/state.json" %(self.outdir)
+        self.output=self.input
+        self.vf=1
+        self.cpu=1
+        self.queue="st.q"
+        self.project=None
     
-    def runclusterjob(self,commandshell,outdirMain,resource=1):
-        pass
+    def runclusterjob(self,commandshell=None,jobname=None):
+        takecommand=commandshell
+        usedjobname=jobname
+        if commandshell is None:
+            takecommand=self.command
+        if jobname is None:
+            usedjobname="test"
+        statedict=self.loadjson()
+        if statedict['control'] == 'run':
+            globalcode=1
+            while(globalcode > 0):
+                recode,submitid=self.submitjob(takecommand,usedjobname)
+                timecap=30
+                if recode == 0:
+                    statedict[usedjobname]=submitid
+                else:
+                    resubmitid=self.rerunjob(recode,timecap,takecommand,usedjobname)
+                    statedict[usedjobname]=resubmitid
+                self.dumpjson(statedict)
+                globalcode=self.checkcomplete(statedict[usedjobname])
+            statedict[usedjobname]='completed'
+            self.dumpjson(statedict)
+        elif statedict['control'] == 'hold':
+            self.hodljob()
+        elif statedict['control'] == 'stop':
+            self.killjob()
+        else:
+            pass
+            
 
-    def submitjob(self, parameter_list):
+
+    def submitjob(self,commandshell,jobname):
+        shelldir="%s/shell/%s" % (self.outdir,jobname)
+        os.makedirs(shelldir,mode=0o755,exist_ok=True)
+        out=open(shelldir+"/"+jobname+".sh",mode='w')
+        out.write(commandshell+"\n")
+        out.write("echo JobFinished")
+        out.close()
+        qsubcommand="qsub -N %s -wd  -l vf=%sG,num_proc=%d -q %s " % (jobname,shelldir,self.vf,self.cpu,self.queue)
+        if self.project is not None:
+            qsubcommand+="-P %s" % (self.project)
+        qsubcommand+=" %s/%s.sh" % (shelldir,jobname)
+        submit=subprocess.Popen(qsubcommand,shell=True,stderr=subprocess.PIPE,stdout=subprocess.PIPE,universal_newlines=True)
+        returncode=submit.wait(timeout=120)
+        if returncode == 0:
+            stdout=submit.stdout.read()
+            stderr=submit.stderr.read()
+            if stderr is not None:
+                return -1,None
+            else:
+                return returncode,stdout
+        else:
+            return 1,None
+    
+    def rerunjob(self,code,timecap,commandshell,jobname):
+        tmpcode=code
+        tmpsid=''
+        totaltime=0
+        while(tmpcode != 0):
+            time.sleep(timecap)
+            tmpcode,tmpsid=self.submitjob(commandshell,jobname)
+            totaltime+=30
+            if totaltime > 7200:
+                tmpcode=0
+                tmpsid='fail to resubmit job for two hours.'
+
+        return tmpsid
+
+    def killjob(self):
+        pass
+    def hodljob(self):
         pass
     def checkalive(self, sgejobid):
         pass
+    def checkcomplete(self, jobid,jobname):
+        if re.match(r'fail',jobid):
+            return 1
+        else:
+            cplcode=-1
+            while(cplcode<0):
+                cmd="qstat -j %s" % jobid
+                stat=subprocess.Popen(cmd,shell=True,stderr=subprocess.PIPE,stdout=subprocess.PIPE,universal_newlines=True)
+                returncode=stat.wait(timeout=120)
+                if returncode==0:
+                    stdout=stat.stdout.read()
+                    stderr=stat.stderr.read()
+                    if re.match(r'Following',stderr):
+                        shelldir="%s/shell/%s/%s.sh.o%s" % (self.outdir,jobname,jobname,jobid)
+                        alllog=open(shelldir,mode='r').readlines()
+                        if re.match(r'JobFinished',alllog[-1]):
+                            cplcode=0
+                time.sleep(60)
+            return cplcode
 
-    def dumpjson(self, outputfile=None):
+    def dumpjson(self,statedict,outputfile=None):
         if outputfile is None:
             outputjson=self.output
         try:
-            out=open(outputjson,mode='w')
-            out.write("{\n")                        
-            for step in self.step:
-                astep=eval(step)
-                stepdict = json.dumps(astep.makedefault('need_fq1','need_fq2'))
-                out.write("%s:%s\n" % (step,stepdict))
-            out.write("outdir:%s" % (os.path.abspath('.')))
-            out.write("}\n")
+            out=open(outputjson,mode='w')                      
+            json.dump(statedict,out)
             out.close()
         except IOError as e:
             raise e
@@ -40,6 +127,6 @@ class jobexecutor:
             inputjson=self.input
         try:
             jsondict=json.load(open(inputjson,mode='r').read())
-        except ValueError as e:
-            raise e
+        except:
+            jsondict={'control':'run'}
         return jsondict
