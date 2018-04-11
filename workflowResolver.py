3a3093469224ce7589f9da34082c170dbba099eb
[huangguodong] <[huangguodong@genomics.cn>
2018-04-11 18:09:26 +0800

    fix workflow resolve detail

--- workflowResolver.py
+++ workflowResolver.py
@@ -6,6 +6,9 @@ import logging
 
 import jobexcutor
 
+logging.getLogger().setLevel(logging.INFO)
+logging.basicConfig(format='%(asctime)s %(message)s')
+
 class workflowResolver():
     def __init__(self):
         self.workflowName=""
@@ -13,33 +16,37 @@ class workflowResolver():
         self.queue="st.q"
         self.project=None
     
-    def loadworkflow(self, workflowName,workflowJson=None):
+    def loadworkflow(self, workflowName,dumpjson,workflowJson=None):
         workflowparser=eval(workflowName).interface()
         jsoncontent={}
-        if workflowJson is None:
+        if dumpjson>0:
             workflowparser.dumpjson()
-            jsoncontent=workflowparser.loadjson()
         else:
-            try:
-                jsoncontent=workflowparser.loadjson(inputfile=workflowJson)
-            except ValueError as e:
-                raise e
-        for step in workflowparser.step:
-            if step in jsoncontent:
-                stepc=eval(workflowName+'.'+step)
-                stepc.parameter=jsoncontent[step]['parameter']
-                stepc.program=jsoncontent[step]['program']
-                stepc.outdirMain=jsoncontent['outdir']
-                commandshell,out1,out2=stepc.makeCommand(jsoncontent[step]['input'][0],jsoncontent[step]['input'][1])
-                logging.info("output:"+out1+"\n"+out2)
-                runjob=jobexcutor.jobexecutor()
-                runjob.outdir=stepc.outdirMain
-                vf,cpu=self.checkjobresource(jsoncontent[step]['resource'])
-                runjob.vf=int(vf)
-                runjob.cpu=int(cpu)
-                runjob.queue=self.queue
-                runjob.project=self.project
-                runjob.runclusterjob(commandshell,step)
+            if workflowJson is None:
+                #workflowparser.dumpjson()
+                jsoncontent=workflowparser.loadjson()
+            else:
+                try:
+                    jsoncontent=workflowparser.loadjson(inputfile=workflowJson)
+                except ValueError as e:
+                    raise e
+            for step in workflowparser.step:
+                if step in jsoncontent:
+                    stepb=eval(workflowName+'.'+step)
+                    stepc=stepb()
+                    stepc.parameter=jsoncontent[step]['parameter']
+                    stepc.program=jsoncontent[step]['program']
+                    stepc.outdirMain=jsoncontent['outdir']
+                    commandshell,out1,out2=stepc.makeCommand(jsoncontent[step]['input'][0],jsoncontent[step]['input'][1])
+                    logging.info("output:"+out1+"\n"+out2)
+                    runjob=jobexcutor.jobexecutor()
+                    runjob.outdir=stepc.outdirMain
+                    vf,cpu=self.checkjobresource(jsoncontent[step]['resource'])
+                    runjob.vf=int(vf)
+                    runjob.cpu=int(cpu)
+                    runjob.queue=self.queue
+                    runjob.project=self.project
+                    runjob.runclusterjob(commandshell,step)
     
     def checkjobresource(self, resource):
         alldecimal=re.findall(r'\d+',resource)
