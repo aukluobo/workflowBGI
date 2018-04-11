f55f77d69b92b626d385bed1e1dec8808760c212
[huangguodong] <[huangguodong@genomics.cn>
2018-04-11 18:08:04 +0800

    fix dumpjson bug

--- Hive.py
+++ Hive.py
@@ -29,7 +29,7 @@ if __name__=='__main__':
     parser.add_argument('--mode',dest='runMode',type=str,help='how to action: run/makejson')
     parser.add_argument('--workflow',dest='workflowName',type=str,help='workflow name to excute,support: WGS')
     parser.add_argument('--outdir',dest='outdir',type=str,default=pwd,help='the output directory,default current directory')
-    parser.add_argument('--inputjson',dest='inputjson',type=str,default='none',help='the user specified json used in workflow')
+    parser.add_argument('--inputjson',dest='inputjson',type=str,default=None,help='the user specified json used in workflow')
     parser.add_argument('--clusterQueue',dest='queue',type=str,default='st.q',help='the queue used to run the workflow, -q in qsub,default st.q')
     parser.add_argument('--projectCode',dest='preject',type=str,help='the project code used to run in queue, -P in qsub')
 
@@ -42,13 +42,16 @@ if __name__=='__main__':
     if localeArg.workflowName is None:
         showhelp("need set --workflow")
         sys.exit()
-    if localeArg.inputjson is None:
+    if localeArg.runMode == 'makejson':
         dumpjson=1
     
+    print(localeArg)
+    print(dumpjson)
+
     startw=workflowResolver.workflowResolver()
     startw.project=localeArg.preject
     startw.queue=localeArg.queue
-    startw.loadworkflow(localeArg.workflowName,localeArg.inputjson)
+    startw.loadworkflow(localeArg.workflowName,dumpjson,localeArg.inputjson)
 
 
 
