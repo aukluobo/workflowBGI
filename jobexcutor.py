d0563b0e85185921e55ab8ee534d78fbe15fde02
[huangguodong] <[huangguodong@genomics.cn>
2018-04-12 17:34:32 +0800

    add checkalive method and change the manage logic in runcluster job

--- jobexcutor.py
+++ jobexcutor.py
@@ -47,14 +47,16 @@ class jobexecutor:
                 self.dumpjson(statedict)
                 time.sleep(30)
                 if globalcode > 0:
-                    globalcode=self.checkcomplete(statedict[usedjobname],usedjobname)
+                    alivecode=self.checkalive(statedict[usedjobname])
+                    if alivecode==0:
+                        globalcode=self.checkcomplete(statedict[usedjobname],usedjobname)
             if globalcode==0:
                 statedict[usedjobname]='completed'
             self.dumpjson(statedict)
         elif statedict['control'] == 'hold':
             self.hodljob()
         elif statedict['control'] == 'stop':
-            self.killjob()
+            pass
         else:
             pass
             
@@ -67,7 +69,7 @@ class jobexecutor:
         out.write(commandshell+"\n")
         out.write("echo JobFinished")
         out.close()
-        qsubcommand="qsub -terse -N %s -wd %s -l vf=%sG,num_proc=%d -q %s " % (jobname,shelldir,self.vf,self.cpu,self.queue)
+        qsubcommand="qsub -terse -N %s.sh -wd %s -l vf=%sG,num_proc=%d -q %s " % (jobname,shelldir,self.vf,self.cpu,self.queue)
         if self.project is not None:
             qsubcommand+="-P %s" % (self.project)
         qsubcommand+=" %s/%s.sh" % (shelldir,jobname)
@@ -77,10 +79,11 @@ class jobexecutor:
         if returncode == 0:
             stdout=submit.stdout.read()
             stderr=submit.stderr.read()
-            if stderr is not None:
+            print(stdout+"\n"+stderr)
+            if stderr is not '':
                 return -1,stderr
             else:
-                return returncode,stdout
+                return returncode,stdout.replace('\n','')
         else:
             return 1,None
     
@@ -98,12 +101,77 @@ class jobexecutor:
 
         return tmpsid
 
-    def killjob(self):
-        pass
+    def killjob(self,sgejobid):
+        cmd='qdel %s' % (sgejobid)
+        subprocess.Popen(cmd,shell=True,universal_newlines=True)
+
+
     def hodljob(self):
         pass
     def checkalive(self, sgejobid):
-        pass
+        cplcode=-1
+        counttime=0
+        tcount=0
+        while(cplcode<0):
+            time.sleep(120)
+            qstatuser="qstat"
+            statall=subprocess.Popen(qstatuser,shell=True,stderr=subprocess.PIPE,stdout=subprocess.PIPE,universal_newlines=True)
+            returncode=-1
+            try:
+                returncode=statall.wait(timeout=120)
+            except:
+                pass
+            if returncode >=0:
+                stdoutall=statall.stdout.readlines()
+                stderrall=statall.stderr.read()
+                if stderrall is not '':
+                    pass
+                else:
+                    jobrecord=[x for x in stdoutall if re.match(re.escape(sgejobid),x)]
+                    if jobrecord:
+                        jobrecordarray=jobrecord[0].split()
+                        if jobrecordarray[4] == 'qw':
+                            pass
+                        elif jobrecordarray[4] == 'Eqw':
+                            self.killjob(sgejobid)
+                            cplcode=1
+                        elif jobrecordarray[4] == 'T':
+                            self.killjob(sgejobid)
+                            cplcode=1
+                        elif jobrecordarray[4] == 'dr':
+                            cplcode=1
+                        elif jobrecordarray[4] == 't':
+                            tcount+=1
+                            if tcount > 5:
+                                cplcode=1
+                        elif jobrecordarray[4] == 'r':
+                            qstatcmd="qstat -j %s" % sgejobid
+                            stat=subprocess.Popen(qstatcmd,shell=True,stderr=subprocess.PIPE,stdout=subprocess.PIPE,universal_newlines=True)
+                            returncode=-1
+                            try:
+                                returncode=stat.wait(timeout=120)
+                            except:
+                                pass
+                            if returncode >=0:
+                                stdout=stat.stdout.readlines()
+                                stderr=stat.stderr.read()
+                                if re.match(r'Following',stderr):
+                                    cplcode=0
+                                else:
+                                    livevf=re.findall(r'veme=\d+',[x for x in stdout if re.match(r'usage',x)][0])
+                                    if livevf >self.vf*1.5 or livevf >self.vf+5:
+                                        counttime+=120
+                                    else:
+                                        counttime=0
+                                    if counttime > 1200:
+                                        newvf=livevf*1.5
+                                        logging.info("jobid %s have break memory for 20min : set %d G used %d G; kill and reqsub new vf %s G" % (sgejobid,self.vf,livevf,newvf))
+                                        self.vf=newvf
+                                        cplcode=1
+                        else:
+                            pass
+
+
     def checkcomplete(self, jobid,jobname):
         if re.match(r'fail',jobid):
             return 1
@@ -112,8 +180,12 @@ class jobexecutor:
             while(cplcode<0):
                 cmd="qstat -j %s" % jobid
                 stat=subprocess.Popen(cmd,shell=True,stderr=subprocess.PIPE,stdout=subprocess.PIPE,universal_newlines=True)
-                returncode=stat.wait(timeout=120)
-                if returncode==0:
+                returncode=-1
+                try:
+                    returncode=stat.wait(timeout=120)
+                except:
+                    pass
+                if returncode >=0:
                     stdout=stat.stdout.readlines()
                     stderr=stat.stderr.read()
                     if re.match(r'Following',stderr):
@@ -127,6 +199,7 @@ class jobexecutor:
                         pass
                     else:
                         pass
+                print(cplcode)
                 time.sleep(60)
             return cplcode
 
