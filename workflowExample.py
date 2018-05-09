cf67cb4caa86ca3aed9b37c073854d826132e9df
[huangguodong] <[huangguodong@genomics.cn>
2018-05-09 20:39:38 +0800

    check outdirMain problem wait the other teammate to get a idea to fix this problem

--- workflowExample.py
+++ workflowExample.py
@@ -11,7 +11,10 @@ class common(object):
         self.ref="/ldfssz1/BC_WGS/pipeline/DNA_Human_WGS_2017b/Database/hg19/hg19.fasta"
 
     def makepair(self, inputfq):
+        pairArray=[]
         pair={}
+        order={}
+        p=0
         for fq in inputfq:
             fqbase=os.path.basename(fq)
             aa=re.search(r'_\d\.fq',fqbase)
@@ -22,7 +25,14 @@ class common(object):
                     pair[prefix][readtype]=fq
                 except:
                     pair[prefix]={readtype:fq}
-        return pair
+                if prefix in order:
+                    pass
+                else:
+                    order[prefix]=p
+                p+=1
+        for pp in sorted(order.items(),key=lambda x : x[1]):
+            pairArray.append(pair[pp[0]])
+        return pairArray
 
 class filter(common):
     def __init__(self):
@@ -30,18 +40,18 @@ class filter(common):
         self.parameter="filter -n 0.1 -q 0.5 -T 1 -l 12 -Q 2 -G -f AAGTCGGAGGCCAAGCGGTCTTAGGAAGACAA -r AAGTCGGATCGTAGCCATGTCGTTCTGTGAGCCAAGGAGTTG -M 2"
         self.program="/ldfssz1/BC_WGS/pipeline/DNA_Human_WGS_2017b/soapnuke/soapnuke_v1.0/bin/SOAPnuke1.5.6"
         self.statprogram="/ldfssz1/BC_WGS/pipeline/DNA_Human_WGS_2017b/soapnuke/soapnuke_v1.0/bin/soapnuke_stat.pl"
-        self.outdir="%s/WGS/cleanfq" % (self.outdirMain)
+        self.outdir="WGS/cleanfq"
         
         
     def makeCommand(self,inputfq):
         pair=self.makepair(inputfq)
         command=""
         output=[]
-        for keys in pair.keys():
-            readc=sorted(pair[keys].keys())
+        for keys in pair:
+            readc=sorted(keys.keys())
             if len(readc)==2:
-                fq1=pair[keys][readc[0]]
-                fq2=pair[keys][readc[1]]
+                fq1=keys[readc[0]]
+                fq2=keys[readc[1]]
                 fq1base=os.path.basename(fq1)
                 fq2base=os.path.basename(fq2)
                 fq1basesub=re.sub(r'_\d\..*',r'',fq1base)
@@ -57,11 +67,11 @@ class filter(common):
         pair=self.makepair(inputfq)
         inputd=[]
         output=[]
-        for keys in pair.keys():
-            readc=sorted(pair[keys].keys())
+        for keys in pair:
+            readc=sorted(keys.keys())
             if len(readc)==2:
-                fq1=pair[keys][readc[0]]
-                fq2=pair[keys][readc[1]]
+                fq1=keys[readc[0]]
+                fq2=keys[readc[1]]
                 fq1base=os.path.basename(fq1)
                 fq2base=os.path.basename(fq2)
                 fq1basesub=re.sub(r'_\d\..*',r'',fq1base)
@@ -75,7 +85,7 @@ class filter(common):
 class alignment(common):
     def __init__(self):
         common.__init__(self)
-        self.parameter=""" mem -t 8 -M -Y -R "@RG\tID:test\tPL:COMPLETE\tPU:lib\tLB:sampleid-WGSPE100\tSM:PE100-2\tCN:BGI" """
+        self.parameter=[""" mem -t 8 -M -Y -R "@RG\tID:test\tPL:COMPLETE\tPU:lib\tLB:sampleid-WGSPE100\tSM:PE100-2\tCN:BGI" """]
         self.program="/ldfssz1/BC_WGS/pipeline/DNA_Human_WGS_2017b/bin/bwa-0.7.15/bwa"
         self.samtools="/ldfssz1/BC_WGS/pipeline/DNA_Human_WGS_2017b/bin/samtools-1.3.1/samtools"      
         self.outdir="%s/WGS/bam" % (self.outdirMain)
@@ -84,18 +94,25 @@ class alignment(common):
         pair=self.makepair(inputfq)
         command=""
         output=[]
-        for keys in pair.keys():
-            readc=sorted(pair[keys].keys())
+        readGroupOrder=0
+        if len(self.parameter) != len(pair):
+            print("read pair and parameter group not equal! will use the last parameter for default!")
+        maxReadGroup=len(self.parameter)
+        for keys in pair:
+            readc=sorted(keys.keys())
             if len(readc)==2:
-                fq1=pair[keys][readc[0]]
-                fq2=pair[keys][readc[1]]
+                fq1=keys[readc[0]]
+                fq2=keys[readc[1]]
                 outprefix=os.path.basename(fq1).split('.')
-                parameter=self.parameter
-                parameter1=parameter.replace("test",outprefix[0]).replace("lib",self.fqLink[outprefix[0]][1]).replace("sampleid",self.fqLink[outprefix[0]][0])
+                parameter1=self.parameter[readGroupOrder]
+                #parameter1=parameter.replace("test",outprefix[0]).replace("lib",self.fqLink[outprefix[0]][1]).replace("sampleid",self.fqLink[outprefix[0]][0])
                 outputbam="%s/%s.bam" % (self.outdir,outprefix[0])
                 command+="%s %s %s %s %s | %s view -Sb -o %s - ;" % (self.program,parameter1,self.ref,fq1,fq2,self.samtools,outputbam)
                 command+="%s sort -@ 8 -O BAM -o %s.sort.bam %s ; %s index %s.sort.bam\n" % (self.samtools,outputbam,outputbam,self.samtools,outputbam)
                 output+=[outputbam,outputbam+".sort.bam"]
+            readGroupOrder+=1
+            if readGroupOrder>maxReadGroup:
+                readGroupOrder=maxReadGroup
         os.makedirs(self.outdir,mode=0o755,exist_ok=True)
         return [command],output
     
@@ -104,12 +121,12 @@ class alignment(common):
         inputq=[]
         output=[]
         parameter=[]
-        for keys in pair.keys():
-            readc=sorted(pair[keys].keys())
+        for keys in pair:
+            readc=sorted(keys.keys())
             if len(readc)==2:
-                fq1=pair[keys][readc[0]]
+                fq1=keys[readc[0]]
                 outprefix=os.path.basename(fq1).split('.')
-                parameter1=self.parameter.replace("test",outprefix[0]).replace("lib",self.fqLink[outprefix[0]][1]).replace("sampleid",self.fqLink[outprefix[0]][0])
+                parameter1=self.parameter[0].replace("test",outprefix[0]).replace("lib",self.fqLink[outprefix[0]][1]).replace("sampleid",self.fqLink[outprefix[0]][0])
                 parameter.append(parameter1)
                 outputbam="%s/%s.bam" % (self.outdir,outprefix[0])
                 output+=[outputbam,outputbam+".sort.bam"]
